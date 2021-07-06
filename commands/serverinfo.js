1c160701046e9c2f2d143ed53a90245846ba5274
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-06 23:53:23 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -10,7 +10,7 @@ module.exports.run = async(bot, message, args) => {
      .setThumbnail(`${message.guild.iconURL({ size: 4096 })}`)
      .setTitle(`${message.guild.name}`)
      .addField("ID:", `${message.guild.id}`)
-     .addField("Owner:", `${message.guild.owner.user.tag}`, true)
+     .addField("Owner:", "Matrixbordje#0213", true)
      .addField("Co-Owner:", "BAP#0515", true)
      .addField("Regio:", `${message.guild.region}`, true)
      .addField("Bots:", `${message.guild.members.cache.filter(m =>m.user.bot).size}`, true)
