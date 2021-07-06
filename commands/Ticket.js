a13403d73aaebe3b74cc70bc7876ae29fac49b02
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-06 23:14:10 +0200

    update

--- commands/Ticket.js
+++ commands/Ticket.js
@@ -65,7 +65,7 @@ module.exports.run = async (bot, message, args) => {
                     .setDescription("Het ticket van " + message.channel.name + " is geopend.")
                     .setColor("#6aa75e")
                     .setFooter('Created by Tweeli.#0001');
-                    var logChannel = message.guild.channels.cache.find(channel => channel.id === "861974224037871616")
+                    var logChannel = message.guild.channels.cache.find(channel => channel.id === "862078473832628314")
                     if(!logChannel) return message.reply("Er is iets misgelopen.");
                     logChannel.send(ticketLog);
    
