3522843fc044e489f6a06e3cafb9e2f74b9e96e4
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-07-11 16:14:18 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,12 +4,12 @@ module.exports.run = async(bot, message, args) => {
 
 
 
-    var commandsEmbed = new discord.MessageEmbed()
+    var inviteEmbed = new discord.MessageEmbed()
      .setTitle("Invites!")
      .addField("Forg. Discord bot server.", "https://discord.gg/c9nfzh6zTu" )
      .setColor("#6aa75e")
      .setFooter('Created by Tweeli.#0001');
-    message.channel.send(commandsEmbed)   
+    message.channel.send(inviteEmbed)   
 
 }
 
